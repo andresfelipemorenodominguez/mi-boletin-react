5b300a9bb47462e1661ead1b2a0f840d8f465169
Andres Felipe <andresdevbod@gmail.com>
2026-05-29 22:54:35 -0500

    corregir errores de vista

--- actions/admin/professors.ts
+++ actions/admin/professors.ts
@@ -1,10 +1,8 @@
 "use server";
 
-import { PrismaClient } from "@prisma/client";
+import { prisma } from "@/lib/prisma";
 import { revalidatePath } from "next/cache";
 import bcrypt from "bcryptjs";
-
-const prisma = new PrismaClient();
 
 export async function getProfessors() {
   try {
